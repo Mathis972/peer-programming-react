27080ccfc3dd8c5d21a0644b7acd30cf69fdf21a
Mathis <eyzex@hotmail.fr>
2022-11-22 16:25:44 +0100

    react router

---
+++ src/components/Hack.tsx
@@ -0,0 +1,16 @@
+const Hack = () => {
+  const onHack = async () => {
+    console.log('null');
+  };
+  return (
+    <div className="w-full max-w-xs">
+      <p>HACK</p>
+      <button onClick={onHack}>OK?</button>
+      <p className="text-center text-gray-500 text-xs">
+        &copy;2022 Ynov Corp. All rights reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Hack;
