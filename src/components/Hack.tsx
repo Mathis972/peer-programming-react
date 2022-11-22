4f4752fecfb7e53771ef804321b0bb70fc3c78de
Mathis <eyzex@hotmail.fr>
2022-11-22 17:33:42 +0100

    display selected contacts

--- src/components/Hack.tsx
+++ src/components/Hack.tsx
@@ -1,14 +1,31 @@
+// @ts-nocheck
+import { useState } from 'react';
+
 const Hack = () => {
+  const [contacts, setContacts] = useState([]);
   const onHack = async () => {
-    console.log('null');
+    const supportedProperties = await navigator.contacts.getProperties();
+
+    const contactsToSelect = await navigator.contacts.select(
+      supportedProperties,
+      {
+        multiple: true,
+      }
+    );
+
+    setContacts(contactsToSelect);
   };
   return (
     <div className="w-full max-w-xs">
       <p>HACK</p>
-      <button onClick={onHack}>OK?</button>
-      <p className="text-center text-gray-500 text-xs">
-        &copy;2022 Ynov. All rights reserved.
-      </p>
+      <button onClick={onHack}>GORS BOUTTON </button>
+      {contacts.map((contact) => (
+        <div>
+          <strong>{contact.name}</strong>
+          <p>{contact.email.join(', ')}</p>
+          <p>{contact.tel.join(', ')}</p>
+        </div>
+      ))}
     </div>
   );
 };
